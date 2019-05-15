bf4b10e9591942010a32b8e86849543dba9b7771
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-15 22:54:30 +0100

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,10 +11,20 @@ class Timer extends Component {
     }
   }
 
-
+//this.props.removeTimer
+//this.props.updateInterval
 
   //Your code here
+  componentDidUpdate() {
+    this.timer.current.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
 
 
   componentDidMount() {
